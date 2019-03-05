ad59277001311c0fbf68d77f0c931dcfd4c30f29
SauSap <tuan.back@classfunc.com>
2019-03-05 13:32:55 +0700

    update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,23 +7,22 @@ function CartPage(props) {
 
    const context = useContext(ShopContext)
 
-   const {cart, removeProductFromCart} = useContext(ShopContext)
+   const {cart} = useContext(ShopContext)
    console.log('Cart props: ', props)
-   console.log('Cart context: ', cart, removeProductFromCart)
 
    return (
       <div>
          <MainNavigation
-            cartItemNumber={context.cart.reduce((count, curItem) => {
+            cartItemNumber={cart.reduce((count, curItem) => {
                console.log(count, curItem)
                return count + curItem.quantity;
             }, 0)}
             path={props.match.path}
          />
          <main className="cart">
-            {context.cart.length <= 0 && <p>No Item in the Cart!</p>}
+            {cart.length <= 0 && <p>No Item in the Cart!</p>}
             <ul>
-               {context.cart.map(cartItem => (
+               {cart.map(cartItem => (
                   <li key={cartItem.id}>
                      <div>
                         <strong>{cartItem.title}</strong> - ${cartItem.price} (
